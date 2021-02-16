66f26ff7d35a6ad5dec6ae11bb3facdff6591f1d
Rima Akter <rimawahid3@gmail.com>
2021-02-17 00:50:44 +0600

    css style

---
+++ css-style.js
@@ -0,0 +1,12 @@
+
+
+
+function addStyles(){
+const add1 = document.querySelector("#para");
+add1.classList.add("para")
+}
+
+function removeStyles(){
+    const add1 = document.querySelector("#para");
+    add1.classList.remove("para")
+    }
